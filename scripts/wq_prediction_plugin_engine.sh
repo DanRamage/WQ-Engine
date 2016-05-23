5597cd1932725b963aec1976f950da88829cd9db
DanRamage <dan@inlet.geol.sc.edu>
2016-05-23 15:13:06 -0400

    Init checkin.

---
+++ scripts/wq_prediction_plugin_engine.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+source /usr/local/virtualenv/pyenv-2.7.11/bin/activate;
+
+cd /home/xeniaprod/scripts/WQ-Engine/scripts;
+
+python wq-prediction-engine.py --PluginDirList=/home/xeniaprod/scripts/MyrtleBeach-Water-Quality/scripts,/home/xeniaprod/scripts/Florida-Water-Quality/scripts --LogConfigFile=/home/xeniaprod/scripts/WQ-Engine/config/prediction_plugin.conf >> /home/xeniaprod/tmp/log/wq_prediction_plugin_engine_sh.log 2>&1
