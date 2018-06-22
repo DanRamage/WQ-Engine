c510cd96c228db345f2b5c8035c0a76de82b9cb9
DanRamage <ChiefDan@gmail.com>
2018-06-22 14:24:58 -0400

    ADded Kill Devil Hills into processing.

--- scripts/wq_prediction_plugin_engine.sh
+++ scripts/wq_prediction_plugin_engine.sh
@@ -4,4 +4,4 @@ source /usr/local/virtualenv/pyenv-2.7.11/bin/activate;
 
 cd /home/xeniaprod/scripts/WQ-Engine/scripts;
 
-python wq-prediction-engine.py --PluginDirList=/home/xeniaprod/scripts/MyrtleBeach-Water-Quality/scripts,/home/xeniaprod/scripts/Florida-Water-Quality/scripts,/home/xeniaprod/scripts/Charleston-Water-Quality/scripts --LogConfigFile=/home/xeniaprod/scripts/WQ-Engine/config/prediction_plugin.conf >> /home/xeniaprod/tmp/log/wq_prediction_plugin_engine_sh.log 2>&1
+python wq-prediction-engine.py --PluginDirList=/home/xeniaprod/scripts/MyrtleBeach-Water-Quality/scripts,/home/xeniaprod/scripts/Florida-Water-Quality/scripts,/home/xeniaprod/scripts/Charleston-Water-Quality/scripts,/home/xeniaprod/scripts/KillDevilHillsWQ/scripts --LogConfigFile=/home/xeniaprod/scripts/WQ-Engine/config/prediction_plugin.conf >> /home/xeniaprod/tmp/log/wq_prediction_plugin_engine_sh.log 2>&1
